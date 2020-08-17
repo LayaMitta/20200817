276a6ed0b956175b7a84b8dce9e7630140a5d129
unknown <mittalaya@gmail.com>
2020-08-17 17:37:31 +0000

    My first version

---
+++ a.py
@@ -0,0 +1,12 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:57:15) [MSC v.1915 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a+b
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    a+b
+NameError: name 'a' is not defined
+>>> print "Hello, World!"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("Hello, World!")?
+>>> print ("Hello, World!")
+Hello, World!
+>>> 
